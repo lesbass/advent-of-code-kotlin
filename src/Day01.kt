33e422a0b528de48bdb95ddd26e78cd6426d94d9
Stefano Maffeis <stefano.maffeis@casavo.com>
2021-12-01 11:04:23 +0100

    resolve Day01 Part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,7 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Int = if (input.isEmpty()) 0
+    else {
+        var prev = input.first().toInt()
+        input
+            .map { it.toInt() }
+            .drop(1)
+            .count {
+                if (it > prev) {
+                    prev = it
+                    true
+                } else {
+                    prev = it
+                    false
+                }
+            }
     }
+
 
     fun part2(input: List<String>): Int {
         return input.size
@@ -9,9 +23,9 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
