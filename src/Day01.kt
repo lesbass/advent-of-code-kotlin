309691b77503f357edfa7d6e2b6d43468df55771
Stefano Maffeis <stefano.maffeis@casavo.com>
2021-12-01 14:20:26 +0100

    feat: resolve Day01 Part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,39 @@
+data class ThreeMeasurement(val first: Int, val second: Int, val third: Int) {
+    fun slide(newThird: Int): ThreeMeasurement = ThreeMeasurement(this.second, this.third, newThird)
+    fun getSum() = first + second + third
+}
+
+data class Counter(val value: Int) {
+    operator fun plus(increment: Int): Counter {
+        return Counter(value + increment)
+    }
+}
+
+data class Measurement(val value: Int)
+
 fun main() {
     fun part1(input: List<String>): Int = input
+        .map { Measurement(it.toInt()) }
+        .foldIndexed(Pair(Counter(0), Measurement(0)))
+        { index, (counter, prev), curr -> Pair(counter + (if (index > 0 && curr.value > prev.value) 1 else 0), curr) }
+        .first.value
+
+    fun part2(input: List<String>): Int = input
         .map { it.toInt() }
-        .foldIndexed(Pair(0, 0))
-        { index, (count, prev): Pair<Int, Int>, i -> Pair(count + (if (index > 0 && i > prev) 1 else 0), i) }
-        .first
-
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+        .foldIndexed(Pair(Counter(0), ThreeMeasurement(0, 0, 0)))
+        { index, (counter, prev), i ->
+            Pair(
+                counter + (if (index > 2 && prev.slide(i).getSum() > prev.getSum()) 1 else 0),
+                prev.slide(i)
+            )
+        }
+        .first.value
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
