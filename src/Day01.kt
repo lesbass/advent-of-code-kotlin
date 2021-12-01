7ecbfd2eafe6653c33fb03ce111f770e09e05c27
Stefano Maffeis <stefano.maffeis@gmail.com>
2021-12-01 22:32:39 +0100

    refactor names

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,29 +1,43 @@
 data class ThreeMeasurement(val first: Int, val second: Int, val third: Int) {
     fun slide(newThird: Int): ThreeMeasurement = ThreeMeasurement(this.second, this.third, newThird)
     fun getSum() = first + second + third
+
+    companion object {
+        fun initial() = ThreeMeasurement(0, 0, 0)
+    }
 }
 
 data class Counter(val value: Int) {
     operator fun plus(increment: Int): Counter {
         return Counter(value + increment)
     }
+
+    companion object {
+        fun initial() = Counter(0)
+    }
 }
 
-data class Measurement(val value: Int)
+data class Measurement(val value: Int) {
+    companion object {
+        fun initial() = Measurement(0)
+    }
+}
 
 fun main() {
     fun part1(input: List<String>): Int = input
         .map { Measurement(it.toInt()) }
-        .foldIndexed(Counter(0) to Measurement(0))
-        { index, (counter, prev), curr ->
-            counter + (if (index > 0 && curr.value > prev.value) 1 else 0) to curr
+        .foldIndexed(Counter.initial() to Measurement.initial())
+        { index, (counter, prevMeasure), currMeasure ->
+            counter + (if (index > 0 && currMeasure.value > prevMeasure.value) 1 else 0) to currMeasure
         }.first.value
 
     fun part2(input: List<String>): Int = input
         .map { it.toInt() }
-        .foldIndexed(Counter(0) to ThreeMeasurement(0, 0, 0))
-        { index, (counter, prev), i ->
-            counter + (if (index > 2 && prev.slide(i).getSum() > prev.getSum()) 1 else 0) to prev.slide(i)
+        .foldIndexed(Counter.initial() to ThreeMeasurement.initial())
+        { index, (counter, prevMeasure), currMeasure ->
+            counter + (if (index > 2 && prevMeasure.slide(currMeasure)
+                    .getSum() > prevMeasure.getSum()
+            ) 1 else 0) to prevMeasure.slide(currMeasure)
         }.first.value
 
     // test if implementation meets criteria from the description, like:
