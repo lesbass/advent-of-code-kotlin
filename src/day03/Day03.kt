781f64833fdc7d1675c0df50472d18d77cc44e69
Stefano Maffeis <stefano.maffeis@casavo.com>
2021-12-03 09:50:26 +0100

    feat: day03 part 2

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -13,24 +13,47 @@ fun main() {
     fun getAverageValueAtPositionInverted(input: List<String>, index: Int) =
         if (getAverageValueAtPosition(input, index) == 1) 0 else 1
 
-    fun getIntFromBinaryString(input: List<Int>): Int = input
-        .joinToString("")
-        .toInt(2)
+    fun getIntFromBinaryString(input: List<Int>) = input.joinToString("").toInt(2)
 
-    fun getGammaRate(input: List<String>) = getIntFromBinaryString(input.first().indices
-        .map { index -> getAverageValueAtPosition(input, index) })
-    fun getEpsilonRate(input: List<String>) = getIntFromBinaryString(input.first().indices
-        .map { index -> getAverageValueAtPositionInverted(input, index) })
+    fun findOxygenGeneratorRating(input: List<String>, prev: String = ""): String =
+        input.filter { it.startsWith(prev) }.let {
+            return@findOxygenGeneratorRating if (it.size == 1) it.first()
+            else findOxygenGeneratorRating(
+                input, prev + getAverageValueAtPosition(
+                    it, prev.length
+                )
+            )
+        }
+
+    fun findCO2ScrubberRating(input: List<String>, prev: String = ""): String =
+        input.filter { it.startsWith(prev) }.let {
+            return@findCO2ScrubberRating if (it.size == 1) it.first()
+            else findCO2ScrubberRating(
+                input, prev + getAverageValueAtPositionInverted(
+                    it, prev.length
+                )
+            )
+        }
+
+    fun getGammaRate(input: List<String>) =
+        getIntFromBinaryString(input.first().indices.map { index -> getAverageValueAtPosition(input, index) })
+
+    fun getEpsilonRate(input: List<String>) =
+        getIntFromBinaryString(input.first().indices.map { index -> getAverageValueAtPositionInverted(input, index) })
+
+    fun getOxygenGeneratorRating(input: List<String>) = findOxygenGeneratorRating(input).toInt(2)
+
+    fun getCO2ScrubberRating(input: List<String>) = findCO2ScrubberRating(input).toInt(2)
 
     fun part1(input: List<String>) = getGammaRate(input) * getEpsilonRate(input)
 
-    fun part2(input: List<String>): Int = input.size
+    fun part2(input: List<String>) = getOxygenGeneratorRating(input) * getCO2ScrubberRating(input)
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day03/Day03_test")
-    check(part1(testInput) == 198)
+    check(part2(testInput) == 230)
 
     val input = readInput("day03/Day03")
     println(part1(input))
-    // println(part2(input))
+    println(part2(input))
 }
