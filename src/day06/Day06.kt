02d4dd5ae23fe466e7e9690c10f0d01d41b4680b
Stefano Maffeis <stefano.maffeis@casavo.com>
2021-12-08 22:56:57 +0100

    rwfactor: Day06

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -8,27 +8,31 @@ fun main() {
     val newFishTimeToChild = oldFishTimeToChild + 2
 
     fun buildLanternFish(counter: Int, qty: Long) = counter to qty
-    fun newLanternFishAndChild(qty: Long) = listOf(buildLanternFish(oldFishTimeToChild, qty), buildLanternFish(newFishTimeToChild, qty))
+    fun newLanternFishAndChild(qty: Long) =
+        listOf(buildLanternFish(oldFishTimeToChild, qty), buildLanternFish(newFishTimeToChild, qty))
+
     fun decreaseLanternFishLife(counter: Int, qty: Long) = listOf(buildLanternFish(counter - 1, qty))
 
-    fun processLanternFish(counter: Int, qty: Long) = if (counter == 0) {
-        newLanternFishAndChild(qty)
-    } else {
-        decreaseLanternFishLife(counter, qty)
+    fun processLanternFish(counter: Int, qty: Long) = when (counter) {
+        0 -> newLanternFishAndChild(qty)
+        else -> decreaseLanternFishLife(counter, qty)
     }
 
     fun List<String>.parseLanternFishes() = first()
         .split(",")
         .groupBy { it.toInt() }
         .map { (counter, qty) -> buildLanternFish(counter, qty.size.toLong()) }
+        .toMap()
+
+    fun processDay(acc: Map<Int, Long>, day: Int) = acc
+        .flatMap { (counter, qty) -> processLanternFish(counter, qty) }
+        .groupBy { (counter) -> counter }
+        .map { (counter, value) -> buildLanternFish(counter, value.sumOf { (_, qty) -> qty }) }
+        .toMap()
 
     fun part(input: List<String>, days: Int): Long = (1..days)
-        .fold(input.parseLanternFishes())
-        { acc, _ ->
-            acc.flatMap { (counter, qty) -> processLanternFish(counter, qty) }
-                .groupBy { (counter) -> counter }
-                .map { (counter, value) -> counter to value.sumOf { (_, qty) -> qty } }
-        }.sumOf { it.second }
+        .fold(input.parseLanternFishes(), ::processDay)
+        .values.sum()
 
     val input = readInput("day06/Day06")
     part(input, 80).apply {
