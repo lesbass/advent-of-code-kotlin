aad1a1c038e739a8dacb16368f57af4b59322437
Stefano Maffeis <stefano.maffeis@casavo.com>
2021-12-11 18:05:52 +0100

    ref: Day11

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -1,13 +1,6 @@
 package day11
 
-import ANSI_GREEN
-import ANSI_RESET
 import readInput
-
-fun Board.replace(octopus: Octopus) =
-    map {
-        if (it.position == octopus.position) octopus else it
-    }
 
 fun main() {
 
@@ -20,27 +13,19 @@ fun main() {
             return newBoard.flashAndCount(flashes + flash)
         } ?: (this to flashes)
 
-    fun Board.applyStep(): Pair<Board, Int> =
-        map { it.raiseLevel() }.flashAndCount()
+    fun Board.applyStep(flashes: Int = 0): Pair<Board, Int> =
+        map { it.raiseLevel() }.flashAndCount(flashes)
 
     fun Board.isAllFlashing() = all { it.energyLevel == 0 }
 
     fun Board.applySteps(n: Int) =
         (1..n).fold(this to 0) { (board, flashes), _ ->
-            board.applyStep().let { x ->
-                x.first to x.second + flashes
-            }
+            board.applyStep(flashes)
         }
 
-    fun Board.findAllFlashing(): Int {
-        var b = this
-        var i = 0
-        while (!b.isAllFlashing()) {
-            b = b.applyStep().first
-            i++
-        }
-        return i
-    }
+    fun Board.findAllFlashing(steps: Int = 0) : Int =
+        if(!isAllFlashing()) applyStep().first.findAllFlashing(steps + 1)
+        else steps
 
     fun part1(input: List<String>) = input.parseData().applySteps(100).second
     fun part2(input: List<String>) = input.parseData().findAllFlashing()
@@ -52,26 +37,34 @@ fun main() {
     }
     part2(testInput).apply {
         println(this)
+        assert(this == 788)
     }
 }
 
 data class Octopus(val position: Coordinates, val energyLevel: Int) {
     fun willFlash() = energyLevel > 9
     fun flashMe(fullData: Board) = if (willFlash())
-        getSurrounding(fullData).fold(fullData) { acc, item ->
-            acc.replace(item.raiseLevel(true))
-        }.replace(copy(energyLevel = 0)) to 1
+        raiseSurrounding(fullData).replace(copy(energyLevel = 0)) to 1
     else fullData to 0
 
     fun raiseLevel(onlyIfNotFlashed: Boolean = false) =
         if (onlyIfNotFlashed && energyLevel == 0) this
         else copy(energyLevel = energyLevel + 1)
 
+    private fun raiseSurrounding(fullData: Board) = getSurrounding(fullData).fold(fullData) { acc, item ->
+        acc.replace(item.raiseLevel(true))
+    }
+
     private fun getSurrounding(fullData: Board) =
         position.getSurrounding(fullData.maxOf { it.position.x + 1 }, fullData.maxOf { it.position.y + 1 })
             .let { positions ->
                 fullData.filter { positions.contains(it.position) }
             }
+
+    private fun Board.replace(octopus: Octopus) =
+        map {
+            if (it.position == octopus.position) octopus else it
+        }
 }
 
 data class Coordinates(val x: Int, val y: Int) {
