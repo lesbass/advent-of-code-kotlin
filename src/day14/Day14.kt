0b70a90c066e77b9fe3ca0f08e67f603827fa83d
Stefano Maffeis <stefano.maffeis@casavo.com>
2021-12-14 14:19:40 +0100

    feat: Day14 part 2

--- src/day14/Day14.kt
+++ src/day14/Day14.kt
@@ -4,41 +4,81 @@ import readInput
 
 fun main() {
 
+    fun List<String>.parseCommands() =
+        drop(1).associate {
+            it.split(" -> ")
+                .let { (pair, elementToAdd) -> pair to elementToAdd }
+        }
+
     fun List<String>.parseData() =
         partition { indexOf(it) < indexOf("") }
-            .let { (first, second) ->
-                first.first() to (second.drop(1).map {
-                    it.split(" -> ")
-                        .let { (pair, elementToAdd) -> pair to elementToAdd }
-                }).toMap()
+            .let { (rawPolymer, rawCommands) ->
+                rawPolymer.first() to rawCommands.parseCommands()
             }
 
-    fun Pair<String, Map<String, String>>.executeSingle() =
+    fun String.growPolymer(dictionary: CommandDict) =
+        (first() + dictionary[this].toString() + drop(1).first()).windowed(2)
+
+    fun List<Pair<String, Long>>.reduceToPolymerMap() = groupBy { (polymer) -> polymer }
+        .map { (polymer, polymerData) -> polymer to polymerData.sumOf { (_, occurrences) -> occurrences } }
+        .toMap()
+
+    fun Pair<PolymerMap, CommandDict>.executeSingle() =
         let { (polymer, dictionary) ->
-            polymer.windowed(2).foldIndexed("") { index, acc, item ->
-                acc + (if (index == 0) item.first() else "") + dictionary[item] + item.drop(1).first()
+            polymer.asIterable()
+                .fold(mapOf<String, Long>()) { polymerMap, (prevPolymer, prevOccurrences) ->
+                    (polymerMap.toList() + prevPolymer.growPolymer(dictionary)
+                        .map { polymer -> polymer to prevOccurrences })
+                        .reduceToPolymerMap()
+                }
+        }
+
+    fun Pair<PolymerMap, CommandDict>.executeTimes(times: Int) =
+        let { (polymerMap, commandDict) ->
+            (1..times).fold(polymerMap) { prevPolymerMap, _ ->
+                (prevPolymerMap to commandDict).executeSingle()
             }
         }
 
-    fun Pair<String, Map<String, String>>.execute(times: Int) =
-        (1..times).fold(first) { acc, _ ->
-            (acc to second).executeSingle()
+    fun String.getEdgeChars() = listOf(first(), last())
+    fun List<Pair<Char, Long>>.getLeastFrequentChar() = maxByOrNull { (_, occurrences) -> occurrences }!!.first
+    fun List<Pair<Char, Long>>.checkIfMinIsInternal(originalPolymer: String) =
+        if (!originalPolymer.getEdgeChars().contains(getLeastFrequentChar())
+        ) 1 else 0
+
+    fun List<Pair<Char, Long>>.getMin(originalPolymer: String) =
+        minOf { (_, occurrences) -> occurrences } / 2 + checkIfMinIsInternal(originalPolymer)
+
+    fun List<Pair<Char, Long>>.getMax() = maxOf { (_, occurrences) -> occurrences } / 2
+    fun List<Pair<Char, Long>>.getResult(originalPolymer: String) = getMax() - getMin(originalPolymer)
+    fun PolymerMap.groupAndCountByChar() =
+        flatMap { (couple, occurrences) -> couple.asIterable().map { char -> char to occurrences } }
+            .groupBy { (char) -> char }
+            .map { (char, charData) -> char to charData.sumOf { (_, occurrences) -> occurrences } }
+
+    fun PolymerMap.getResult(originalPolymer: String) =
+        groupAndCountByChar().getResult(originalPolymer)
+
+    fun List<String>.execute(n: Int) = parseData()
+        .let { (polymer, dictionary) ->
+            (polymer.windowed(2).groupBy { it }
+                .map { (couple, occurrences) -> couple to occurrences.size.toLong() }.toMap() to dictionary)
+                .executeTimes(n).getResult(polymer)
         }
 
-    fun String.getResult() = asIterable().groupBy { it }.let {
-        it.maxOf { x -> x.value.size } - it.minOf { x -> x.value.size }
+    fun part1(input: List<String>) = input.execute(10)
+    fun part2(input: List<String>) = input.execute(40)
+
+    val testInput = readInput("day14/Day14")
+    part1(testInput).apply {
+        println(this)
+        assert(this == 2194.toLong())
     }
-
-    fun part1(input: List<String>) = input.parseData().execute(10).getResult()
-    fun part2(input: List<String>) = input.parseData().execute(40).getResult()
-
-    val testInput = readInput("day14/Day14_test")
-    //part1(testInput).apply {
-    //    println(this)
-        //assert(this == 790)
-    //}
     part2(testInput).apply {
         println(this)
+        assert(this == 2360298895777)
     }
-
 }
+
+typealias CommandDict = Map<String, String>
+typealias PolymerMap = Map<String, Long>
